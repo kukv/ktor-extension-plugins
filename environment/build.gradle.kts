d6d05717d6edc2e9fe38d2dfaddf70c332b52f6c
kukv <rio.by.kukv@gmail.com>
2023-03-06 02:23:53 +0900

    Revert "update"
    This reverts commit 1ffbb7349c08e84be12894b91d3675fb07540810.

--- environment/build.gradle.kts
+++ environment/build.gradle.kts
@@ -6,7 +6,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    `maven-publish`
 }
 
 dependencies {
@@ -29,31 +28,5 @@ tasks {
     }
     withType<Test> {
         useJUnitPlatform()
-    }
-
-    val sourcesJar by creating(Jar::class) {
-        val sourceSets: SourceSetContainer by project
-        archiveClassifier.set("sources")
-        from(sourceSets["main"].allSource)
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
-
-            from(components["kotlin"])
-
-            artifact(tasks["sourcesJar"])
-
-            pom {
-                name.set("ktor-extension-plugins")
-                description.set("ktor-extension-plugins")
-                url.set("https://github.com/kukv/ktor-extension-plugins")
-            }
-        }
     }
 }
