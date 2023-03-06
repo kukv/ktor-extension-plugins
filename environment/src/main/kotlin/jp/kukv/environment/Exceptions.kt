073e4a657a906972a62b78063119465a39d10e3e
kukv <rio.by.kukv@gmail.com>
2023-03-06 22:37:41 +0900

    update

--- environment/src/main/kotlin/jp/kukv/environment/Exceptions.kt
+++ environment/src/main/kotlin/jp/kukv/environment/Exceptions.kt
@@ -1,4 +1,4 @@
 package jp.kukv.environment
 
 internal class KeyNotFoundException(message: String) : RuntimeException(message)
-internal class NotInitializedException(message: String) : RuntimeException(message)
+internal class InitializationFailureException(message: String) : RuntimeException(message)
