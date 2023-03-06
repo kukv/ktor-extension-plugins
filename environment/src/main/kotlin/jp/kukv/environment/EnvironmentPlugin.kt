5d393e1d82ff38f2891c9d34be4c28c0cc5c719e
kukv <rio.by.kukv@gmail.com>
2023-03-06 10:55:01 +0900

    remove public

--- environment/src/main/kotlin/jp/kukv/environment/EnvironmentPlugin.kt
+++ environment/src/main/kotlin/jp/kukv/environment/EnvironmentPlugin.kt
@@ -3,11 +3,11 @@ package jp.kukv.environment
 import io.ktor.server.application.createApplicationPlugin
 import io.ktor.server.config.ApplicationConfig
 
-public class EnvironmentConfiguration {
+class EnvironmentConfiguration {
     var config: ApplicationConfig? = null
 }
 
-public val EnvironmentPlugin = createApplicationPlugin(name = "Environment", ::EnvironmentConfiguration) {
+val EnvironmentPlugin = createApplicationPlugin(name = "Environment", ::EnvironmentConfiguration) {
 
     val environment = Environment.init(pluginConfig.config)
     EnvironmentContext.register(environment)
