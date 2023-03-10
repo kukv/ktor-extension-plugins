6a14fdd77bbd6566c7b133208a08b895ecb627c3
kukv <rio.by.kukv@gmail.com>
2023-03-10 19:15:51 +0900

    refactor

--- environment/build.gradle.kts
+++ environment/build.gradle.kts
@@ -2,11 +2,8 @@
 // https://github.com/gradle/gradle/issues/23784
 @file:Suppress("DSL_SCOPE_VIOLATION")
 
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    id("org.jetbrains.dokka")
 }
 
 dependencies {
@@ -20,13 +17,8 @@ dependencies {
     testImplementation(kotlin("test-junit5"))
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+tasks {
+    withType<Test> {
+        useJUnitPlatform()
     }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
