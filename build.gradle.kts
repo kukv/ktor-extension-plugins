e9fbf848ac4ae24f1dfeab82ad2e0869b84a9310
kukv <rio.by.kukv@gmail.com>
2023-03-11 13:08:40 +0900

    不要な依存関係の定義を削除

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,3 @@ allprojects {
         mavenCentral()
     }
 }
-dependencies {
-    implementation("com.h2database:h2:2.1.214")
-}
--- gradle-scripts/build.gradle.kts
+++ gradle-scripts/build.gradle.kts
@@ -7,5 +7,4 @@ dependencies {
     implementation(libs.dokka.gradle.plugin)
     implementation(libs.spotless.gradle.plugin)
     implementation(libs.nexus.publish.plugin)
-    implementation("com.h2database:h2:2.1.214")
 }
