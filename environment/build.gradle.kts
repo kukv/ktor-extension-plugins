3735a5fe09e3430fc2520dd52ebf32cf30c84a97
kukv <rio.by.kukv@gmail.com>
2023-04-07 12:56:59 +0900

    tomlファイルで依存関係を管理するのをやめてみる

--- environment/build.gradle.kts
+++ environment/build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
 }
 
 dependencies {
-    implementation(libs.ktor.server.core)
-    implementation(libs.kotlinx.datetime)
+    implementation("io.ktor:ktor-server-core:2.2.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
 }
