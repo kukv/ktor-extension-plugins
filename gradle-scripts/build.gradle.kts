26b2a2b7e9e22bc87b1af097b9a4872d701bd9f7
Koki Nonaka <43878809+kukv@users.noreply.github.com>
2023-07-29 20:17:20 +0900

    Update build.gradle.kts
    Signed-off-by: Koki Nonaka <43878809+kukv@users.noreply.github.com>

--- gradle-scripts/build.gradle.kts
+++ gradle-scripts/build.gradle.kts
@@ -7,8 +7,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.8.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.8.20")
     implementation("com.diffplug.spotless:spotless-plugin-gradle:6.20.0")
     implementation("io.github.gradle-nexus:publish-plugin:1.3.0")
 }
