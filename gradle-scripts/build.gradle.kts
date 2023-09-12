62dd82086a3a701f08e044c84797c40471e98f15
kukv <rio.by.kukv@gmail.com>
2023-09-12 12:33:38 +0900

    update dependency library

--- gradle-scripts/build.gradle.kts
+++ gradle-scripts/build.gradle.kts
@@ -7,8 +7,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.8.20")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.0")
     implementation("com.diffplug.spotless:spotless-plugin-gradle:6.21.0")
     implementation("io.github.gradle-nexus:publish-plugin:1.3.0")
 }
