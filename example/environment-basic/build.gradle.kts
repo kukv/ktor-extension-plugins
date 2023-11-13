2452a9e4cc9e314f781ff513e292bfff36ada375
kukv <rio.by.kukv@gmail.com>
2023-11-13 21:26:57 +0900

    kotlin-moduleをやめる

--- example/environment-basic/build.gradle.kts
+++ example/environment-basic/build.gradle.kts
@@ -1,5 +1,8 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 plugins {
-    id("kotlin-module")
+    application
+    kotlin("jvm") version "1.9.10"
     kotlin("plugin.serialization") version "1.9.10"
 }
 
@@ -15,4 +18,28 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.1")
 
     implementation("jp.kukv.ktor-extension-plugins:environment:0.1.2")
+}
+
+application {
+    mainClass.set("jp.kukv.example.ApplicationKt")
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of("17"))
+    }
+}
+
+tasks {
+    withType<KotlinJvmCompile> {
+        kotlinOptions {
+            jvmTarget = "17"
+            apiVersion = "1.8"
+            languageVersion = "1.8"
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
 }
